4341ef29c69f3158ce7acee6ce5a2ab8c4462bed
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-12-29 18:58:22 +0100

    Update classe.py

--- libs/paddle/classe.py
+++ libs/paddle/classe.py
@@ -12,16 +12,19 @@ class Paddle():
         if event.x - 30 <= 0:
             self.Game.canevas.coords(self.paddle, 0, 500, 60, 500 + 8)
             if self.Game.ball.static:
+                self.Game.root.bind("<Button-1>", self.Game.ball.launch_ball)
                 self.Game.canevas.coords(self.Game.ball.ball, 20, 480,
-                                        40, 480 + 20)
+                                         40, 480 + 20)
 
         elif event.x + 30 > 800:
             self.Game.canevas.coords(self.paddle, 740, 500, 800, 500 + 8)
             if self.Game.ball.static:
+                self.Game.root.bind("<Button-1>", self.Game.ball.launch_ball)
                 self.Game.canevas.coords(self.Game.ball.ball, 760, 480,
-                                        780, 480 + 20)
+                                         780, 480 + 20)
         else:
             self.Game.canevas.coords(self.paddle, event.x - 30, 500, event.x + 30, 500 + 8)
             if self.Game.ball.static:
+                self.Game.root.bind("<Button-1>", self.Game.ball.launch_ball)
                 self.Game.canevas.coords(self.Game.ball.ball, event.x - 10, 480,
-                                        event.x + 10, 480 + 20)
+                                         event.x + 10, 480 + 20)

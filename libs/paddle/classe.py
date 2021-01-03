5612095c55d0135a08f4f862a0d10243ca2c5fbb
Eliott-Lepage <64136394+Eliott-Lepage@users.noreply.github.com>
2021-01-03 15:35:19 +0100

    Update classe.py

--- libs/paddle/classe.py
+++ libs/paddle/classe.py
@@ -1,14 +1,39 @@
-from tkinter import *
-
-
 class Paddle:
+    """
+    Class representing a paddle
+       Author : Mathis Dory, Eliott Lepage
+       Date : November 2020
+       This class is used to create an animate Paddle which moves from right to left
+    """
     def __init__(self, game):
+        """
+        Creation of all the features of the paddle : width, height, movement speed
+        Bind <motion> for the object paddle : it moves when the user moves the mouse
+
+        PRE:
+            Game class is running
+        POST:
+           - Creation of the paddle label which represents the paddle in the Graphic User Interface
+           - Binding of the movement of the paddle
+        RAISES :
+            -
+        """
         self.Game = game
         self.dx = 1
         self.paddle = self.Game.canevas.create_rectangle(370, 500, 370 + 60, 500 + 8, fill='grey')
         self.Game.root.bind("<Motion>", self.motion)
 
     def motion(self, event):
+        """
+        This function changes the coordinates of the paddle when the user moves the mouse and launch the ball
+
+        PRE : event is the action of moving the paddle from right to left
+        POST :
+            - It modifies the coordinates of the paddle
+            - Call self.Game.ball.launch_ball : the ball is launched from the paddle
+        RAISES :
+            -
+        """
         if event.x - 30 <= 0:
             self.Game.canevas.coords(self.paddle, 0, 500, 60, 500 + 8)
             if self.Game.ball.static:

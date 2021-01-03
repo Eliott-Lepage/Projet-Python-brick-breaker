aced01d1e818da0604a673c798f37ae3123cb372
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2021-01-03 13:44:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 from libs.fenetre.classe import Window
-from libs.brique.classe import Brick
-from tkinter import *
+from mvp import display_console
+import sys
 
-if __name__ == "__main__":
-    def startProgram(method="GUI"):
-        if method == "GUI":
-            fenetre = Window()
-            fenetre.window.mainloop()
-        elif method == "console":
-            Brick.display_console(self="")
+'''Program developed on the basis of the game Brick Breaker. To run the program, the user must type this command
+ in a terminal: python main.py <parameter>
+- The parameter must be "console" to launch the program in MVP
+- The parameter must be "GUI" to launch the program in graphical interface
 
+Projet Python EPHEC Janvier 2021
+Groupe 2TM1-2
+03/01/21
 
+@:param argv[1] is used to determine the version of the game ( MVP or GUI )
+@:type string
 
-    # Ecrire "console" comme paramètre pour lancer en mode console.
-    startProgram("console")
+'''
+
+__author__ = "Mathis Dory, Eliott Lepage"
+__version__ = "1.0.0"
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "GUI":
+            gui = Window()
+            gui.window.mainloop()
+        elif sys.argv[1] == "console":
+            display_console()
+        else:
+            print("Introduisez console pour lancer en mode MVP\nIntroduisez GUI pour lancer en GUI")
+    else:
+        print("Le programme nécessite 1 paramètre: console ou GUI !")
